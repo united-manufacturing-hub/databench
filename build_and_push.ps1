5f84dc43f8690d09389ab6f2763434a685ea101c
Ferdinand Linnenberg <fl@umh.app>
2023-09-12 15:54:40 +0200

    fix: move go code to golang folder

--- build_and_push.ps1
+++ build_and_push.ps1
@@ -1,5 +1,9 @@
-docker buildx build -t ghcr.io/united-manufacturing-hub/databench:latest -f .\Dockerfile .
-docker push ghcr.io/united-manufacturing-hub/databench:latest
+docker buildx build -t ghcr.io/united-manufacturing-hub/databench:latest -f .\golang\Dockerfile .
+docker push ghcr.io/united-manufacturing-hub/databench:latest-golang
+
+docker buildx build -t ghcr.io/united-manufacturing-hub/databench:latest -f .\rust\Dockerfile .
+docker push ghcr.io/united-manufacturing-hub/databench:latest-rust
+
 git add .
 git commit -m "feat: updated image"
 git push
